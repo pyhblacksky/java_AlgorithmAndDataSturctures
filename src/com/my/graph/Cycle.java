77a2cdaeafeb6762be1e87ab2999a50c3b63488a
pyh <664254320@qq.com>
2018-07-23 20:40:59 +0800

    add new class

---
+++ src/com/my/graph/Cycle.java
@@ -0,0 +1,42 @@
+package com.my.graph;
+
+/*	
+ * 	无环图：一个无回路的图
+ * 	功能：判断是G是否为无环图
+ * 		假设不存在自环或平行边
+ * 
+ * */
+
+public class Cycle {
+
+	private boolean[] marked;
+	private boolean hasCycle;
+	
+	public Cycle(Graph G)
+	{
+		marked = new boolean[G.V()];
+		for(int s = 0; s < G.V(); s++)
+		{
+			if(!marked[s])
+				dfs(G, s, s);	//	起点和终点均为自己，返回真，则说明存在环
+		}
+	}
+	
+	public void dfs(Graph G, int v, int u)
+	{
+		marked[v] = true;
+		for(int w : G.adj(v))	//	对于v的每一个相邻点来说
+		{
+			if(!marked[w])
+				dfs(G, w, v);
+			else if(w != u)		//	 其余点均已标记，v和u不是邻接点，说明存在一条路
+				hasCycle = true;
+		}
+	}
+	
+	public boolean hasCycle()
+	{
+		return hasCycle;
+	}
+	
+}
---
+++ src/com/my/graph/TwoColor.java
@@ -0,0 +1,46 @@
+package com.my.graph;
+
+/*
+ * 	功能：判断是否为偶图（二分图）——双色问题
+ * 
+ * */
+
+public class TwoColor {
+
+	private boolean[] marked;
+	private boolean[] color;
+	private boolean isTwoColorable = true;
+	
+	public TwoColor(Graph G)
+	{
+		marked = new boolean[G.V()];
+		color = new boolean[G.V()];
+		for(int s = 0; s < G.V(); s++)	//	对于G的每一个点
+		{
+			if(!marked[s])
+				dfs(G, s);	
+		}
+	}
+	
+	
+	//	DFS
+	private void dfs(Graph G, int v)
+	{
+		marked[v] = true;
+		for(int w : G.adj(v))	//	对于图G的每一个邻接点来说
+		{
+			if(!marked[w])
+			{
+				color[w] = !color[v];
+				dfs(G, v);
+			}
+			else if(color[w] == color[v])	//	对于已标记的点
+				isTwoColorable = false;
+		}
+	}
+	
+	public boolean isBipartite()
+	{
+		return isTwoColorable;
+	}
+}
