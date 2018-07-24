3f2d3ea4cbb39dadf2012062bc27656b8ec612c6
pyh <664254320@qq.com>
2018-07-24 09:31:14 +0800

    add new class

---
+++ src/com/my/graph/SymbolGraph.java
@@ -0,0 +1,22 @@
+package com.my.graph;
+
+import com.algs.api.ST;
+
+
+/*
+ * 	符号图的数据类型
+ * 	
+ * 	以往的图是用数字作为结点，此处实现的目标是：以字符作为结点的图
+ * 	实现图需要索引和反向索引，即	符号名->索引	索引->符号名	
+ * 
+ * */
+
+public class SymbolGraph {
+	
+	private ST<String, Integer> st;
+	
+	public SymbolGraph(String stream, String sp)
+	{
+		
+	}
+}
